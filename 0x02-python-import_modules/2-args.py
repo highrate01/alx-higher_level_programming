be33f8b65e41f350678c4b41e074e5d199ca65e7
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2023-11-30 13:01:28 -0800

    prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     arg_line = sys.argv
     line_size = len(arg_line) - 1
 
-    if line_size == 0:
-        print("{} arguments".format(line_size))
-    elif line_size > 1:
+    if line_size > 1:
         print("{} arguments:".format(line_size))
         for i in range(1, line_size + 1):
             print("{}: {}".format(i, arg_line[i]))
+    elif line_size == 0:
+        print("{} argument".format(line_size))
     else:
         print("{} argument:".format(line_size))
         print("{}: {}".format(1, arg_line[1]))
