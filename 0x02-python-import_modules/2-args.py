7e9baf93478fe7e05c8518d8a8ff6d89e8ce9434
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2023-11-30 12:03:36 -0800

    prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-def lists(av):
+def arg_line(av):
     number = len(av) - 1
-
     if number == 0:
         print("0 arguments.")
     elif number == 1:
         print("1 argument:")
     else:
         print("{:d} arguments:".format(number))
-
     i = 1
     while i <= number:
         print("{:d}: {}".format(i, av[i]))
@@ -16,4 +14,4 @@ def lists(av):
 
 if __name__ == "__main__":
     import sys
-    lists(sys.argv)
+    arg_line(sys.argv)
