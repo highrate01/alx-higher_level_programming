fc8105772116cde68003847f34b799ef756f686c
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2023-12-02 13:51:08 -0800

    Replace element

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
-       #updated_list = my_list[:]
         my_list[idx] = element
-        #return updated_list
     return my_list
