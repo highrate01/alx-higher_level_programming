8fad45f1ddda3c706c1077c3b8851a6931bf6828
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-01-22 00:39:46 -0800

    replace at particular index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+    temp = my_list[:]
+    if idx < 0:
+        return temp
+    if idx >  len(my_list) - 1:
+        return temp
+    temp[idx] = element
+    return temp
