f99833a7430ae5910446859ce0642a0bec9efa9c
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-01-22 06:13:43 -0800

    print the result of addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-if __name == "__main__":
-    from sys import argv
-    sumint = 0
-    for i in range(1, len(argv)):
-        sumint += int(argv[1])
-    print("{}".format(sumint))
+def add_args(argv):
+    number = len(argv) - 1
+    if number == 0:
+        print("{:d}".format(number))
+    else:
+        i = 1
+        add = 0
+        while i <= number:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
+if __name__ == "__main__":
+    import sys
+    add_args(sys.argv)
