92e1226b2725685bba2fae5501c4c45bbb9e86a8
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-01-22 05:46:09 -0800

    corrected

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    arg_line = sys.argv
-    line_size = len(arg_line) - 1
-    if line_size > 1:
-        print("{} arguments:".format(line_size))
-        for i in range(1, line_size + 1):
-            print("{}: {}".format(i, arg_line[i]))
-    elif line_size == 0:
-        print("{} argument".format(line_size))
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{} arguments:".format(x))
+    elif x == 1:
+        print("{} arguments:".format(x))
     else:
-        print("{} argument:".format(line_size))
-        print("{}: {}".format(1, arg_line[1]))
+        print("{} arguments:".format(x))
+    for i in range(x):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
