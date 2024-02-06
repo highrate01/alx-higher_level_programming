0077d93a91505d20fbbd22aa24c73fac862636d0
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-06 13:24:48 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a Class Square."""
+
+class Square:
+    """Represent a Square."""
+    def __init__(self, size):
+
+        """Initialise a new square.
+        Args:
+        size(int): The size of the
+        new square"""
+        self.__size = size
