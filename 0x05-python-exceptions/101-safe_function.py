96fd9845cd14b209174fc2d50c8eb9d885927df1
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-06 07:25:09 -0800

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except (ValueError, ZeroDivisionError, TypeError, IndexError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
