af7540705138fd343a54021f40be47ef9d875b1e
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-07 06:23:38 -0800

    3-square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,7 +14,7 @@ class Square:
         size(int): The size of the new square
         """
 
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
