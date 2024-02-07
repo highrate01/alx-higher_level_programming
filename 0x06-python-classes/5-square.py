43dd811f6db40b52e6546166ee04341ddd1f10eb
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-07 07:17:33 -0800

    5-square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -27,6 +27,7 @@ class Square:
     def area(self):
         """Return the current area of the square"""
         return self.__size * self.__size
+
     def my_printf(self):
         """print the square with # character"""
         for i in range(0, self.__size):
