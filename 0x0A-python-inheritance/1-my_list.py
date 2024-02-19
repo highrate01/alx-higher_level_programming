a9ca67b09842ec1580aae73b46823d212190e4e5
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-19 09:00:17 -0800

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an inherited list"""
+
+
+class Mylist(list):
+    """implement a sorted printing for the buily-in list class"""
+
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
