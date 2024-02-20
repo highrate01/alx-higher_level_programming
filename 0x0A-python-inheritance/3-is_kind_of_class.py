e7faa32f0872d9d13a76d698993ab8ef6d4ea2ab
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-19 20:41:36 -0800

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class and inherited class-checking  function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if an obeject is an instance or inherited instance of a class.
+    Args:
+        obj (any): The object to check.
+        a_clss (type): The class to match the type of obj to.
+    Returns:
+        if obj is an instance or inherited instance of a_class - True,
+        otherwise - false.
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
