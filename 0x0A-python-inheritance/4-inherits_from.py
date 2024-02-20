365e620adebce29b51459c84e820dba3fa122473
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-20 03:59:18 -0800

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines an inherited class ''checking function''."""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False.
+    Args:
+        obj: The object check.
+        a_class (type): The claass to match the type of obj to.
+    Returns:
+        if obj is an inherited instance of a_class - True. Otherwise, False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
