1e907b64055ae0f17413ccc0b868035aed76f1b1
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-21 13:20:27 -0800

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a string-to-json function"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object
+    (string)
+    """
+    return json.dumps(my_obj)
