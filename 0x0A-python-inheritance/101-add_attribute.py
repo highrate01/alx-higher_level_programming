2f8b9a55723311fe83682cf65e57d6a2a846cc70
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-20 16:12:37 -0800

    add attr

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function that adds attributes to object."""
+
+
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible.
+    Args:
+        obj (any): The object to an attribute to
+        att (str): The name of the attribute to add
+        value (any): The value of the attribute
+    Returns:
+        TypeError: if the attriute cannot be added.
+    """
+    if not hasattr(obj, "__diet__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value
