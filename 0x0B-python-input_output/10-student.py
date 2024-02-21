3610e5b741936dbf2a5aa863cd473a6d240dd21b
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-21 15:20:50 -0800

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -27,4 +27,4 @@ class Student:
                 all(type(element) == str for element in attrs)):
             return {k: getattr(self, k) for k in attrs if
                     hasattr(self, k)}
-            return self.__dict__
+        return self.__dict__
