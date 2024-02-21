11d566f1d5782a922ae6e40f8680c06d4748e4d4
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-21 13:59:38 -0800

    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a JSON-object function"""
+
+
+def from_json_string(my_str):
+    """Returns the python object representation of a JSON
+    string"""
+    return json.loads(my_str)
