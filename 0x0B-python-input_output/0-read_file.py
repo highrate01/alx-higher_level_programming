bbc4ed31a75030f5b964cdc708e4eee7e32ed6dc
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-21 12:38:45 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Prints the content of a UTF-8 file to stdout"""
-    with open(filename, encoding="utf-8")as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(),end="")
