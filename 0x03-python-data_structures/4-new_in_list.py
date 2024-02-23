f354de30d2fb982c80590cec61e751bed487b2cd
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-23 06:36:52 -0800

    updated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     temp = my_list[:]
-    if idx == 0:
+    if idx < 0:
         return temp
     elif idx > len(my_list) - 1:
         return temp
