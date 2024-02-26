4b0fa14196d37b2205b2e66941cb2c239dc5c74c
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-26 03:21:25 -0800

    Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,8 +13,24 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        The getter
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """The setter"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Represents str
         """
-        return "[square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[square] ({}) {}/{} - {}".format(self.id,
+                self.x,
+                self.y,
+                self.width)
