4802b918a72e90f9c1956539fb2016e4b6e90a19
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-26 13:34:09 -0800

    updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,9 @@ class Rectangle(Base):
     Rectangle class for base model
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        class constructor
+        """
         super().__init__(id)
 
         self.width = width
