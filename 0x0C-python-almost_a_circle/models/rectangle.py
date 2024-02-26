7f23f82d730aed199a830b2fe216b055f5031a88
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-26 13:14:11 -0800

    check date

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """
-    Rectangle class
+    Rectangle class for base model
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
