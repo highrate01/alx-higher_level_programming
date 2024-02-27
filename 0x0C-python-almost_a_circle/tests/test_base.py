33c2b90b2ffd216de62abb19ada6e89f7a49a39f
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-27 10:47:00 -0800

    updated

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines unittest for base
+"""
+import unittest
+from models.rectangle import Rectangle
+from models.square import Square
+
+class Test_base_create(Unittest.TestCase):
+    """Testing for the Base Class"""
+
+    def test_rectangle_create(self):
