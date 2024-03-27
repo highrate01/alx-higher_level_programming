12b5bbbd682b889a5311d2611246f695cf6f7da6
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-03-27 08:15:15 +0000

    sql intro

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,2 @@
-/*lists all databases of your MySQL serve*/
-SHOW GRANTS FOR 'highrate12'@'localhost';
-SHOW GRANTS FOR 'highrate'@'localhost';
+-- lists all databases of your MySQL server.
+SHOW DATABASES;
