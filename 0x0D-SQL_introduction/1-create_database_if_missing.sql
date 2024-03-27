71eb8c29c22099b44e0974b9a945fa964320ad8a
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-03-27 08:02:49 +0000

    create database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,3 @@
 /*creates the database hbtn_0c_0 in your MySQL server*/
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
-CREATE USER IF NOT EXISTS 'highrate12'@'localhost' IDENTIFIED BY 'highrate';
-
-GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO 'highrate12'@'localhost';
