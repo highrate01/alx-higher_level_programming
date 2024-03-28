a43fea45072458ab5845c3ed65a6f703a2f950a7
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-03-28 16:59:53 +0000

    more queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+
+SELECT b.title, a.genre_id FROM tv_show_genres a
+RIGHT JOIN tv_shows b ON a.show_id = b.id
+ORDER BY b.title, a.genre_id ASC;
