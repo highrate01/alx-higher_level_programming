f0838738d9040035af9ab622588c7b8d790af249
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-04-24 20:58:04 +0100

    i love c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const arg2 = process.argv[2];
 const x = parseInt(arg2);
 
 if (isNaN(x)) {
-  console.log('Number not found');
+  console.log('Missing number of occurences');
 } else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
