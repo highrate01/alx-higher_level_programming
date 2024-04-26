856a477ca3557d06e63bfb3a9dad02d652eb8e24
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-04-26 21:20:29 +0000

    sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-import sys
+"""
+list states and save from sql injection
+"""
+from sys import argv
 import MySQLdb
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    stateName = sys.argv[4]
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    stateName = argv[4]
 
     db = MySQLdb.connect(
             host='localhost', port=3306, user=username,
             passwd=password, db=database, charset="utf8"
             )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (stateName,))
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cursor.execute(query, (stateName, ))
     states = cursor.fetchall()
     for state in states:
         print(state)
