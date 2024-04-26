73b2b7f5537ce805faa9caafdc756e5107d06fa3
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-04-26 12:28:52 +0000

    SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    stateName = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=username,
+            passwd=password, db=database, charset="utf8"
+            )
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cursor.execute(query, (stateName, ))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
