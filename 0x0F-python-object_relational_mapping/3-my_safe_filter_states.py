78d7ae6d17d6aad2ee6c07ef200f0fd6efa156db
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-04-26 21:12:23 +0000

    sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             passwd=password, db=database, charset="utf8"
             )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cursor.execute(query, (stateName,))
     states = cursor.fetchall()
     for state in states:
