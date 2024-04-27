7daab8b85995516b439ca8251de05ce251166c92
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-04-27 14:29:57 +0000

    contains a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+lists all State objects that contain the letter
+a from the database
+"""
+from sys argv
+from sqlalchemy import (create_engine)
+from sqlalchemy  import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                username, password, database), pool_ping=True
+            )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(binf=engine)
+    session = Session()
+
+    for state in session.query(State).filter(
+            state.name.like('%a%')).order_by(state.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
