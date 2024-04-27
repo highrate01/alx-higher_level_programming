c8c51ae750a5d22083635d502f4dba752400adc3
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-04-27 22:45:48 +0000

    deletes states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,7 +5,7 @@ letter a from the database hbtn_0e_6_usa
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_base import Base, State
+from model_state import Base, State
 from sys import argv
 
 
@@ -15,6 +15,7 @@ if __name__ == "__main__":
                 argv[1], argv[2], argv[3]), pool_pre_ping=True
             )
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).filter(State.name.like('%a%')).all()
 
