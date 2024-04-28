9a6586e628031bea88c25a42b7889f10e26ef499
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-04-28 09:27:05 +0000

    City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+contains the call definition of City
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """
+    Defines class representing the table
+    """
+
+    __tablename__ = 'cities'
+
+    id = Column(
+                Integer, primary_key=True,
+                autoincrement=True, nullable=False,
+                unique=True
+                )
+    name = Column(String(128), nullable=False)
+    state_id = Column(
+                      Integer, ForeignKey("states.id"),
+                      nullable=False
+                      )
