a5e3abee461830281881e4f965eacf4272a05ef3
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-04-30 06:40:31 +0000

    list relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,9 +20,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     res = session.query(City, State).filter(City.state_id == State.id).all()
-
     for city, state in res:
-
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.commit()
