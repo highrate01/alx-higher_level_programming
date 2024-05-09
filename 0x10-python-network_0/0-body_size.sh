85fdeea4c1dfed90d4faefa0e4fe0cd237800191
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-05-09 23:14:46 +0000

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
-
-if [ -z "$1" ]; then
-	echo "Usage: $0 <url>"
-	exit 1
-fi
-
-response=$(curl -s "$1")
-
-body_size=${#response}
-
-echo "$body_size"
-
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
