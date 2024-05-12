8b3f5b2d81d9645f40bc6108d32de9ceb4fa247c
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-05-12 13:12:24 +0000

    mygithub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     auth = HTTPBasicAuth(argv[1], argv[2])
 
     res = requests.get("https://api.github.com/user", auth=auth)
