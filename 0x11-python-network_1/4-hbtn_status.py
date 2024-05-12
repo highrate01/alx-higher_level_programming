d13265f691c37b52dd130b630dfdedd172bf78f1
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-05-12 08:15:22 +0000

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,6 +8,6 @@ import requests
 if __name__ == "__main__":
     fetch = requests.get("https://alx-intranet.hbtn.io/status")
 
-    print("Body responses:")
+    print("Body respone:")
     print("\t- type: {}".format(type(fetch.text)))
     print("\t- content: {}".format(fetch.text))
