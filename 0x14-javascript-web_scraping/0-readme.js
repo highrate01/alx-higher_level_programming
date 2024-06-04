aa6aa89fa6accf3b9079cb3e277c1f96dfce69e4
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-04 20:22:34 +0000

    read_me

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-
-	if (error) {
-		console.error('Error reading the file:', error);
-	} else {
-		console.log(content);
-	}
+fs.readFile(process.argv[2], 'utf8', (error, content) => {
+  if (error) {
+    console.error('Error reading the file:', error);
+  } else {
+    console.log(content);
+  }
 });
